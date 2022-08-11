a540b87d08354f9fee132a28ed2abe5e94a495a7
Arbalest <499624095@qq.com>
2022-08-11 15:09:35 +0800

    chore(vite): vite@3 import.meta.globEager remove

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
-const files = import.meta.globEager('./modules/**/*.ts')
+const files: Record<string, any> = import.meta.glob('./modules/**/*.ts', { eager: true })
 const modules: Record<string, any> = {}
 Object.keys(files).forEach(key => {
     const moduleName = key.replace(/\.\/modules.*\//, '').replace('.ts', '')
